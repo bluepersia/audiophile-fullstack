d3dca1e12c2cff3ed8dceebd2b63783bb86a8fb6
Marco Enrique <fuzzyslash@hotmail.com>
2026-08-22 13:42:20 -0600

    Add categories section

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import type { JSX } from "react/jsx-runtime";
 import Hero from "./Hero/Hero";
 import styles from "./Home.module.scss";
+import Categories from "../../components/Categories/Categories";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <div className={styles.hero}>
         <Hero />
+      </div>
+      <div className={styles.categories}>
+        <Categories />
       </div>
     </>
   );
