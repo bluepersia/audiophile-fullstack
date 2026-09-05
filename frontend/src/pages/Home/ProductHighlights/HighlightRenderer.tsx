a87e989358cb75211f1264bddbd935723efe4c50
Marco Enrique <fuzzyslash@hotmail.com>
2026-09-04 18:05:57 -0600

    Add homepage product highlight two

--- frontend/src/pages/Home/ProductHighlights/HighlightRenderer.tsx
+++ frontend/src/pages/Home/ProductHighlights/HighlightRenderer.tsx
@@ -4,6 +4,7 @@ import { getProductBySlug, type ProductData } from "../../../api/products";
 import HighlightOne from "./HighlightOne/HighlightOne";
 import { useQuery } from "@tanstack/react-query";
 import ProgQuery from "../../../components/ProgQuery/ProgQuery";
+import HighlightTwo from "./HighlightTwo/HighlightTwo";
 
 type HighlightRendererProps = {
   highlight: SectionData;
@@ -26,6 +27,9 @@ export default function HighlightRenderer({
       {(product: ProductData) => {
         if (highlight.sectionType === 1)
           return <HighlightOne highlight={highlight} product={product} />;
+
+        if (highlight.sectionType === 2)
+          return <HighlightTwo highlight={highlight} product={product} />;
 
         return <></>;
       }}
---
+++ frontend/src/pages/Home/ProductHighlights/HighlightTwo/HighlightTwo.tsx
@@ -0,0 +1,40 @@
+import type { JSX } from "react/jsx-runtime";
+import Btn from "../../../../components/Btn/Btn";
+import { DESKTOP_BP, TABLET_BP } from "../../../../consts/breakpoints";
+import { createProductLink } from "../../../../core/linkCreation";
+import type { HighlightProps } from "../HighlightRenderer";
+import styles from "./HighlightTwo.module.scss";
+import clsx from "clsx";
+
+export default function HighlightTwo({
+  highlight,
+  product,
+}: HighlightProps): JSX.Element {
+  return (
+    <article className={styles.highlightTwo}>
+      <h2 className={clsx(styles.title, "h4")}>{product.name}</h2>
+      <Btn
+        color="transparent"
+        to={createProductLink(product.slug)}
+        aria-label={`View ${product.name}`}
+      >
+        See Product
+      </Btn>
+      <picture className={styles.picture}>
+        <source
+          srcSet={highlight.image.desktop}
+          media={`(min-width:${DESKTOP_BP}px)`}
+        />
+        <source
+          srcSet={highlight.image.tablet}
+          media={`(min-width:${TABLET_BP}px)`}
+        />
+        <img
+          src={highlight.image.mobile}
+          alt={highlight.alt}
+          className={styles.img}
+        />
+      </picture>
+    </article>
+  );
+}
