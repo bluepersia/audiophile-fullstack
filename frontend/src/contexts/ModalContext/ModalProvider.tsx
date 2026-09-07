0b9945a178a820add29cc66131282cd9f6d867b8
Marco Enrique <fuzzyslash@hotmail.com>
2026-09-06 19:51:08 -0600

    Close modal on page switch

--- frontend/src/contexts/ModalContext/ModalProvider.tsx
+++ frontend/src/contexts/ModalContext/ModalProvider.tsx
@@ -1,11 +1,17 @@
-import { useState, type JSX, type PropsWithChildren } from "react";
+import { useEffect, useState, type JSX, type PropsWithChildren } from "react";
 import type { ModalType } from "./ModalContext.types";
 import { ModalContext } from "./ModalContext";
+import { useLocation } from "react-router";
 
 export default function ModalProvider({
   children,
 }: PropsWithChildren): JSX.Element {
+  const location = useLocation();
   const [currentModal, setCurrentModal] = useState<ModalType>(null);
+
+  useEffect(() => {
+    closeModal();
+  }, [location.pathname]);
 
   function toggleModal(type: ModalType) {
     if (currentModal?.type === type?.type) {
